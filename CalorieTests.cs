686b99f6c4b8a842712fcc908ebb522edf9c4a21
Animeology <joseph.nguyen19@yahoo.com>
2023-07-05 14:39:39 -0500

    Update CalorieTests.cs

--- CalorieTests.cs
+++ CalorieTests.cs
@@ -2,6 +2,20 @@
 {
     public class CalorieTests
     {
+        static void Main(string[] args)
+        {
 
+        }
+
+        static void CalculateMenDeficitCalorie_ReturnExpected()
+        {
+            Info.m_weight = 10;
+
+            Calorie menDeficit = new Calorie();
+
+            menDeficit.MenCalorieDeficit();
+
+            float expected = menDeficit.BMR;
+        }
     }
 }
