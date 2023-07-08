0c9f509eb91486290de3bcadeeca5ecc792f7af4
Animeology <joseph.nguyen19@yahoo.com>
2023-07-08 18:51:22 -0500

    BenchPR tested

--- PersonalRecordTests.cs
+++ PersonalRecordTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fitness
+﻿namespace Fitness
 {
-    internal class PersonalRecordTests
+    public class PersonalRecordTests
     {
+        static void Main(string[] args)
+        {
+            BenchPR_ReturnExpected();
+        }
+
+        static void BenchPR_ReturnExpected()
+        {
+            PersonalRecord.m_bench = 135.0f;
+            PersonalRecord pr = new PersonalRecord();
+
+            var expectedBench = PersonalRecord.m_bench;
+            var actualBench = pr.BenchPR();
+
+            if(actualBench == expectedBench)
+            {
+                Console.WriteLine("BenchPR returns the correct output");
+            }
+            else
+            {
+                Console.WriteLine("BenchPR returns the incorrect output");
+            }
+        }
     }
 }
